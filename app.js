3cd5c2ea31a9fef14992dcaee84bd2ac566dd016
최해일 <bighaeil@gmail.com>
2023-08-20 01:01:16 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 80
 
+app.get('/health', (req, res) => {
+  res.send('OK')
+})
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
